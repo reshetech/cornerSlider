4cec65696a2e5e809548b838b71d8945bd16efbe
reshetech <yossefbh@gmail.com>
2015-03-07 06:26:55 +0200

    Making it an official jQuery plugin

---
+++ cornerslider.jquery.json.js
@@ -0,0 +1,31 @@
+{
+    "name": "cornerSlider",
+    "title": "jQuery CornerSlider",
+    "description": "CornerSlider is a jQuery plugin that slides content in and out the page when the user scrolls down and up the page.",
+    "keywords": [
+        "slider"
+    ],
+    "version": "1.0.0",
+    "author": {
+        "name": "Yossef Benharosh",
+        "url": "http://reshetech.co.il/en/yossef-benharosh/resume/"
+    },
+    "maintainers": [
+        {
+            "name": "Yossef Benharosh",
+            "email": "support@reshetech.co.il"
+        }
+    ],
+    "licenses": [
+        {
+            "type": "MIT",
+            "url": "https://github.com/jquery/jquery-color/blob/2.1.2/MIT-LICENSE.txt"
+        }
+    ],
+    "homepage": "http://reshetech.co.il/en/jquery-plugins/corner-slider",
+    "docs"    : "http://reshetech.co.il/en/jquery-plugins/corner-slider",
+    "download": "http://reshetech.co.il/en/jquery-plugins/corner-slider",
+    "dependencies": {
+        "jquery": ">=1.7"
+    }
+}
